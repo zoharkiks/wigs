213554454c83895533bd8767d8b818a46d9f83d5
zohar <zoharkiks@gmail.com>
2021-07-11 15:08:23 +0530

    Added favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div className="flex">
       <Head>
         <title>Wigs</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />  
       </Head>
 
-    <Sidebar/>
-    <div className="flex flex-col w-screen">
-    <Navbar/>
-    <Hero/>
+      <Sidebar />
+      <div className="flex flex-col w-screen">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
-    
-    </div>
-  )
+  );
 }
